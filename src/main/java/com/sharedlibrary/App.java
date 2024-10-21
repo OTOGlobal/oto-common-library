73cae062b53c5111c4eb94dcbd13e620c91f4e4c
Sena Selışık <selisik@tryoto.com>
2024-10-21 14:02:19 +0300

    OTO-112937

---
+++ src/main/java/com/sharedlibrary/App.java
@@ -0,0 +1,14 @@
+package com.sharedlibrary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static final String SHARED_VARIABLE = "test";
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
